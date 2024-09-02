f01c76130b19f1c209f84bdfa32f2c740cf19254
SharuSonu <75562227+SharuSonu@users.noreply.github.com>
2024-09-02 14:11:36 +0530

    updated print
    Updated A4 Print

--- src/Frontend/utils/authUtils.js
+++
@@ -1,9 +0,0 @@
-// authUtils.js
-
-// Check if the user is authenticated
-export const isAuthenticated = () => {
-    // Logic to determine if the user is authenticated (e.g., checking for authentication token)
-    const authToken = localStorage.getItem('token');
-    return !!authToken; // Convert to boolean value
-  };
-  
